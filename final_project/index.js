3ffbbf1733562a8c96170b50d53108cd9e996ae4
heva31 <vandanatrivedi2156@gmail.com>
2025-06-28 16:41:07 +0530

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+     if (!req.session.authorization) {
+        return res.status(403).json({ message: "User not logged in." });
+    }
+
+    const token = req.session.authorization.token;
+
+    jwt.verify(token, "access", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid token." });
+        }
+        req.user = user; // Save the decoded user info
+        next();
+    });
 });
  
 const PORT =5000;
